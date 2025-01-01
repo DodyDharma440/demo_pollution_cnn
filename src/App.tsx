0911590363fa8603c6ad43abae6db06060de577b
Dodi Aditya <dodiaridharma@gmail.com>
2025-01-01 18:08:42 +0800

    handle select camera

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,26 @@ const App = () => {
   const [prediction, setPrediction] = useState("");
   const [confidence, setConfidence] = useState(0);
 
+  const [cameraOptions, setCameraOptions] = useState<MediaDeviceInfo[]>([]);
+  const [selectedCamera, setSelectedCamera] = useState("");
+
+  useEffect(() => {
+    const getDevices = async () => {
+      const data = await navigator.mediaDevices.enumerateDevices();
+      const devices = data.filter((d) => d.kind === "videoinput");
+      setCameraOptions(devices);
+      const selected = localStorage.getItem("selectedCamera");
+
+      if (selected) {
+        setSelectedCamera(selected);
+      } else {
+        setSelectedCamera(devices[0].deviceId);
+        localStorage.setItem("selectedCamera", devices[0].deviceId);
+      }
+    };
+    getDevices();
+  }, []);
+
   useEffect(() => {
     const loadModel = async () => {
       const loadedModel = await tf.loadLayersModel(
@@ -30,24 +50,26 @@ const App = () => {
 
   // Setup kamera
   useEffect(() => {
-    const setupCamera = async () => {
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-          });
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
+    if (selectedCamera) {
+      const setupCamera = async () => {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+              video: { deviceId: selectedCamera },
+            });
+            if (videoRef.current) {
+              videoRef.current.srcObject = stream;
+            }
+          } catch (error) {
+            console.error("Error accessing camera:", error);
           }
-        } catch (error) {
-          console.error("Error accessing camera:", error);
+        } else {
+          console.error("getUserMedia not supported on this browser.");
         }
-      } else {
-        console.error("getUserMedia not supported on this browser.");
-      }
-    };
-    setupCamera();
-  }, []);
+      };
+      setupCamera();
+    }
+  }, [selectedCamera]);
 
   const predictFrame = async () => {
     if (videoRef.current && model) {
@@ -86,6 +108,21 @@ const App = () => {
       <h1 className="text-xl mb-4 font-bold text-center">
         Air Pollution Prediction
       </h1>
+      <div className="mb-4">
+        <label>Select camera</label>
+        <select
+          value={selectedCamera}
+          onChange={(e) => {
+            setSelectedCamera(e.target.value);
+            localStorage.setItem("selectedCamera", e.target.value);
+          }}
+          className="border border-gray-200 px-4 py-2 w-full appearance-none rounded-lg"
+        >
+          {cameraOptions.map((opt) => {
+            return <option value={opt.deviceId}>{opt.label}</option>;
+          })}
+        </select>
+      </div>
       <video
         ref={videoRef}
         autoPlay
